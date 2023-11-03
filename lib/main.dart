f042ce12661dd2f2699ef3f89a83987137a53b32
Luis Felipe <luisfelipearaujopimenta@gmail.com>
2023-11-02 22:04:10 -0300

    List

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,112 @@
-import 'package:flutter/material.dart';
-import 'myapp.dart';
+import 'dart:async';
+import 'dart:convert';
 
-void main() {
-  runApp(MyApp());
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<List<Photo>> fetchPhotos(http.Client client) async {
+  String url = 'https://economia.awesomeapi.com.br/json/daily/USD-BRL/15';
+  final response = await client
+      .get(Uri.parse(url));
+  print(response.body);
+  // Use the compute function to run parsePhotos in a separate isolate.
+  return compute(parsePhotos, response.body);
+}
+
+// A function that converts a response body into a List<Photo>.
+List<Photo> parsePhotos(String responseBody) {
+  final parsed =
+  (jsonDecode(responseBody) as List).cast<Map<String, dynamic>>();
+
+  return parsed.map<Photo>((json) => Photo.fromJson(json)).toList();
+}
+
+class Photo {
+
+  final String title;
+  final String url;
+
+
+  const Photo({
+
+    required this.title,
+    required this.url
+  });
+
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return Photo(
+
+      title: json['high'] as String,
+      url: json['low'] as String,
+    );
+  }
+}
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const appTitle = 'Isolate Demo';
+
+    return const MaterialApp(
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: FutureBuilder<List<Photo>>(
+        future: fetchPhotos(http.Client()),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text('An error has occurred!'),
+            );
+          } else if (snapshot.hasData) {
+            return PhotosList(photos: snapshot.data!);
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class PhotosList extends StatelessWidget {
+  const PhotosList({super.key, required this.photos});
+
+  final List<Photo> photos;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: ListView.builder(
+          itemCount: photos.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+                title: Text(photos[index].title),
+                subtitle: Text(photos[index].title)
+            );
+          }),
+    );
+  }
 }
